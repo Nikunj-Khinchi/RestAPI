5091e42a5c9a3163f060f545e203727a01551eed
Nikunj-Khinchi <nikunjkhinchi@gmail.com>
2023-09-20 01:03:04 +0530

    Csv format added and route changed

--- controllers/product.js
+++ controllers/product.js
@@ -1,72 +1,148 @@
 const products = require("../models/product");
+const json2csv = require("json2csv").parse;
 
-exports.getAllProducts = async(req, res) => {
-   const { company, name, featured, sort, select, format } = req.query;
-   const queryObject = {};
-   if (company) {
-      queryObject.company = company;
-   }
-   if (name) {
-      queryObject.name = { $regex: name, $options: "i" };
-   }
-   if (featured) {
-      queryObject.featured = featured;
-   }
-   let apiData = products.find(queryObject);
+exports.getAllProducts = async (req, res) => {
+  const { company, name, featured, sort, select, format } = req.query;
+  const queryObject = {};
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  if (featured) {
+    queryObject.featured = featured;
+  }
 
-   if (sort) {
-      let sortFix = sort.split(",").join(" ");
-      apiData = apiData.sort(sortFix);
-   }
+  let apiData = products.find(queryObject);
 
-   // Apply field selection based on the 'select' query parameter
-   let selectedFields = ["id", "name", "price", "featured", "company" , "createdAt"]; // Default fields
+  if (sort) {
+    let sortFix = sort.split(",").join(" ");
+    apiData = apiData.sort(sortFix);
+  }
 
-   if (select) {
-      selectedFields = select.split(",").map(field => field.trim());
-   }
+  // Apply field selection based on the 'select' query parameter
+  let selectedFields = [
+    "id",
+    "name",
+    "price",
+    "featured",
+    "company",
+    "createdAt",
+  ]; // Default fields
 
-   // Pagination
-   let page = Number(req.query.page) || 1;
-   let limit = Number(req.query.limit) || 10;
-   let skip = (page - 1) * limit;
+  if (select) {
+    selectedFields = select.split(",").map((field) => field.trim());
+  }
 
-   apiData = apiData.skip(skip).limit(limit);
+  // Pagination
+  let page = Number(req.query.page) || 1;
+  let limit = Number(req.query.limit) || 10;
+  let skip = (page - 1) * limit;
 
-   apiData.then((products) => {
-      if (format && format.toLowerCase() === 'xml') {
-         // Send response in XML format with selected fields
-         const xmlResponse = `<Api>${products.map(product => {
+  apiData = apiData.skip(skip).limit(limit);
 
-            const selectedData = selectedFields.map(field => `<${field}>${product[field]}</${field}>`).join('');
+  apiData
+    .then((products) => {
+      if (format && format.toLowerCase() === "xml") {
+        // Send response in XML format with selected fields
+        const xmlResponse = `<Api>${products
+          .map((product) => {
+            const selectedData = selectedFields
+              .map((field) => `<${field}>${product[field]}</${field}>`)
+              .join("");
 
             return `<product>${selectedData}</product>`;
-            
-         }).join('')}</Api>`;
+          })
+          .join("")}</Api>`;
 
-         res.set('Content-Type', 'application/xml');
+        res.set("Content-Type", "application/xml");
 
-         res.status(200).send(xmlResponse);
+        res.status(200).send(xmlResponse);
+      } else if (format && format.toLowerCase() === "csv") {
+        // Send response in CSV format with selected fields
+        const selectedProducts = products.map((product) => {
+          const selectedData = {};
+          selectedFields.forEach(
+            (field) => (selectedData[field] = product[field])
+          );
+          return selectedData;
+        });
+        const csvData = json2csv(selectedProducts);
+        res.set("Content-Type", "text/csv");
+        res.status(200).send(csvData);
       } else {
-         // Send response in JSON format with selected fields
-         const selectedProducts = products.map(product => {
-
-            const selectedData = {};
-
-            selectedFields.forEach(field => selectedData[field] = product[field]);
-
-            return selectedData;
-         });
-         
-         res.status(200).json({ products: selectedProducts, nbHits: selectedProducts.length });
+        // Send response in JSON format with selected fields
+        const selectedProducts = products.map((product) => {
+          const selectedData = {};
+          selectedFields.forEach((field) => (selectedData[field] = product[field]));
+          return selectedData;
+        });
+        res.status(200).json({
+          products: selectedProducts,
+          nbHits: selectedProducts.length,
+        });
       }
-   }).catch((err) => {
+    })
+    .catch((err) => {
       throw err;
-   });
-}
+    });
+};
 
-exports.getAllProductsTesting = async(req,res) =>{
-   const myData  = await products.find(req.query).sort("name");
-   console.log(req.query);
-    res.status(200).json({myData , nbHits : myData.length});
- }
+exports.getAllProductsTesting = async (req, res) => {
+  let selectedFields = [
+    "id",
+    "name",
+    "price",
+    "featured",
+    "company",
+    "createdAt",
+  ];
+  const { format } = req.query;
+
+  const queryObject = {};
+  if (format) {
+    queryObject.format = format;
+  }
+
+  products
+    .find({})
+    .sort("name")
+    .then((data) => {
+      if (format && format.toLowerCase() === "csv") {
+        // Send response in CSV format with selected fields
+        const selectedProducts = data.map((product) => {
+          const selectedData = {};
+          selectedFields.forEach(
+            (field) => (selectedData[field] = product[field])
+          );
+          return selectedData;
+        });
+
+        const csvData = json2csv(selectedProducts);
+        res.attachment("products.csv");
+        res.status(200).send(csvData);
+      }else if (format && format.toLowerCase() === "xml") {
+        // Send response in XML format with selected fields
+        const xmlResponse = `<Api>${data
+          .map((product) => {
+            const selectedData = selectedFields
+              .map((field) => `<${field}>${product[field]}</${field}>`)
+              .join("");
+
+            return `<product>${selectedData}</product>`;
+          })
+          .join("")}</Api>`;
+
+        res.set("Content-Type", "application/xml");
+
+        res.status(200).send(xmlResponse);
+      }
+       else {
+        res.status(200).json({ data, nbHits: data.length });
+      }
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
